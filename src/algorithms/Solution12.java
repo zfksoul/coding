0794872566a1515084ce9d44a7a78c1041302a84
zfksoul <407720777@qq.com>
2019-10-18 19:43:55 +0800

    test1

---
+++ src/algorithms/Solution12.java
@@ -0,0 +1,16 @@
+package algorithms;
+
+/**
+* 
+*
+* @author zfk
+* @date 2019年10月18日 下午7:42:58 
+* 
+*/
+public class Solution12 {
+	public static void main(String[] args) {
+		int[] array = {3,4,5,6,1,2};
+		Solution6 so = new Solution6();
+		System.out.println(so.minNumberInRotateArray(array));
+	}
+}
