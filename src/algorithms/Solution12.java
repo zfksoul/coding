8487556ba3f26effba530395c8e51bf5377c1a87
zfksoul <407720777@qq.com>
2019-10-19 15:19:34 +0800

    second commit

--- src/algorithms/Solution12.java
+++ src/algorithms/Solution12.java
@@ -10,10 +10,24 @@ package algorithms;
 public class Solution12 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int i;
-		int x;
+		Solution12 so = new Solution12();
+		System.out.println(so.Power(2.2,3));
 	}
+	public double Power(double base, int exponent) {
+		if(exponent==0) {
+			return 1;
+		}else if(exponent==1) {
+			return base;
+		}else {
+			double result = Power(base,exponent>>1);
+			result *= result;
+			if((exponent&1)==1) {
+				result *= base;
+			}
+			return result;
+		}
+		
+   }
 	
 
 }
