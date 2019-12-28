40684ff2404e0014544a79a7db916d9e82c55fff
zfksoul <407720777@qq.com>
2019-12-28 20:09:18 +0800

    new commit

--- src/binarysearch/BinarySearch.java
+++ src/binarysearch/BinarySearch.java
@@ -1,18 +1,15 @@
 package binarysearch;
 
 /**
- * 
- *
  * @author zfk
  * @date 2019年10月26日 上午9:33:22
- * 
  */
 public class BinarySearch {
     /*
-     * 当我们将区间[l, r]划分成[l, mid]和[mid + 1, r]时， 
+     * 当我们将区间[l, r]划分成[l, mid]和[mid + 1, r]时，
      * 其更新操作是r = mid或者l = mid + 1，
      * 计算mid时不需要加1。
-     * 
+     *
      * 找第一个大于target的数
      */
     public int binarySearch1(int l, int r, int target, int[] arr) {
@@ -28,10 +25,10 @@ public class BinarySearch {
     }
 
     /*
-     * 当我们将区间[l, r]划分成[l, mid - 1]和[mid, r]时， 
+     * 当我们将区间[l, r]划分成[l, mid - 1]和[mid, r]时，
      * 其更新操作是r = mid - 1或者l = mid，
      * 此时为了防止死循环，计算mid时需要加1。
-     * 
+     *
      * 找第一个小于等于target的数
      */
     public int binarySearch2(int l, int r, int target, int[] arr) {
