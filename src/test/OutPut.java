28965e73d80e7441582fb732fa1b4cf29e3fe2ad
zfksoul <407720777@qq.com>
2020-01-13 09:51:18 +0800

    commit on book

---
+++ src/test/OutPut.java
@@ -0,0 +1,30 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OutPut {
+    public static void main(String[] args){
+        List<People> list = new ArrayList<>();
+        list.add(new People(1));
+        list.add(new People(1));
+        list.add(new People(1));
+        System.out.println(list);
+    }
+}
+class People{
+    String name;
+    int age;
+    public People(int age){
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "People{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
+
