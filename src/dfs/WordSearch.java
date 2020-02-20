b41d12da1ad49994e01236d0533e41647cd190f9
zfksoul <407720777@qq.com>
2020-02-20 23:53:36 +0800

    new commit

--- src/dfs/WordSearch.java
+++ src/dfs/WordSearch.java
@@ -11,9 +11,7 @@ import java.util.List;
  * O(nm*3^k)
  */
 public class WordSearch {
-    int n,m;
-    int[] dx = {-1,0,1,0};
-    int[] dy = {0,1,0,-1};
+
     public static void main(String[] args){
         char[][] board = {{'a','b','c'},
                           {'d','e','f'},
@@ -23,28 +21,43 @@ public class WordSearch {
     }
     public boolean exist(char[][] board, String word){
         if (board == null || board[0] == null) return false;
-        n = board.length;
-        m = board[0].length;
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < m; j++){
-                if (dfs(board,i,j,word,0)) return true;
+        int[] dx = {-1,0,1,0};
+        int[] dy = {0,1,0,-1};
+        for (int i = 0; i < board.length; i++){
+            for (int j = 0; j < board[0].length; j++){
+                if (dfs1(board,i,j,word,dx,dy,0)) return true;
             }
         }
         return false;
     }
 
-    private boolean dfs(char[][] board, int x, int y, String word, int u) {
-        if (board[x][y] != word.charAt(u)) return false;
-        if (u == word.length() - 1) return true;
+    private boolean dfs1(char[][] board, int x, int y, String word, int[] dx, int[] dy, int n) {
+        if (board[x][y] != word.charAt(n)) return false;
+        if (n == word.length() - 1) return true;
+        board[x][y] = '.';
+        for (int i = 0; i < dx.length; i++){
+            int a = x + dx[i];
+            int b = y + dy[i];
+            if (a >= 0 && a < dx.length && b >= 0 && b < dy.length){
+                if (dfs1(board, a, b, word, dx, dy, n + 1)) return true;
+            }
+        }
+        board[x][y] = word.charAt(n);
+        return false;
+    }
+
+    private boolean dfs(char[][] board, int x, int y, String word,int[] dx, int[] dy, int n) {
+        if (board[x][y] != word.charAt(n)) return false;
+        if (n == word.length() - 1) return true;
         board[x][y] = '.';          //用过该位置标记不能再用
         for (int i = 0; i < 4; i++){
             int a = x + dx[i];
             int b = y + dy[i];
-            if (a >= 0 && a < n && b >= 0 && b < m){
-                if (dfs(board, a, b, word, u+1)) return true;
+            if (a >= 0 && a < board.length && b >= 0 && b < board[0].length){
+                if (dfs(board, a, b, word, dx, dy, n+1)) return true;
             }
         }
-        board[x][y] = word.charAt(u);//回溯，恢复初始状态
+        board[x][y] = word.charAt(n);//回溯，恢复初始状态
         return false;
     }
 }
