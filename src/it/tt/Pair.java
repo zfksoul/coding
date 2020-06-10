8932f98f38a65b19322aa9c4655ad35a41274b92
zfksoul <407720777@qq.com>
2020-06-11 00:18:24 +0800

    new commit

---
+++ src/it/tt/Pair.java
@@ -0,0 +1,27 @@
+package it.tt;
+
+import java.util.Objects;
+
+public class Pair{
+    int x;
+    int y;
+    public Pair(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair pair = (Pair) o;
+        return x == pair.x &&
+                y == pair.y;
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(x, y);
+    }
+}
--- src/it/tt/bfs.java
+++ src/it/tt/bfs.java
@@ -190,12 +190,5 @@ public class bfs {
 
     }
 }
-class Pair{
-    int x;
-    int y;
-    public Pair(int x, int y){
-        this.x = x;
-        this.y = y;
-    }
-}
+
 
